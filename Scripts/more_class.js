47b46ca9b44513c4c30123f2dbcaaae509c1ae27
cimasome <143644209+cimasome@users.noreply.github.com>
2024-08-17 14:52:45 -0300

    Add files via upload

---
+++ Scripts/more_class.js
@@ -0,0 +1,185 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const todasAsCategoriasDiv = document.getElementById('todasascategorias');
+    const categoriasContainer = document.getElementById('categorias-container');
+    const bannerProdutos = document.querySelector('.banner-produtos');
+    const popupCategorias = document.getElementById('popup-categorias');
+    const popupCategoriasContainer = document.getElementById('popup-categorias-container');
+    const closePopup = document.querySelector('.popup-close-button');
+    const verTodosProdutosButton = document.getElementById('ver-todos-produtos');
+    const bannerProdutosContainer = document.getElementById('banner-produtos-container');
+
+    let classeEscolhida = '';
+
+    function criarCategoriasDinamicamente(data) {
+        // Determinar o número de classes aleatórias a exibir
+        const classes = [...new Set(data.map(produto => produto.Classe))];
+        let numeroDeClasses = 4; // Valor padrão
+
+        // Ajustar o número de classes com base na largura da tela
+        if (window.matchMedia('(max-width: 428px)').matches) {
+            numeroDeClasses = 5;
+        }
+
+        const classesAleatorias = [];
+
+        while (classesAleatorias.length < numeroDeClasses && classes.length > 0) {
+            const randomIndex = Math.floor(Math.random() * classes.length);
+            classesAleatorias.push(classes.splice(randomIndex, 1)[0]);
+        }
+
+        // Limpar o conteúdo atual da banner-produtos
+        bannerProdutosContainer.innerHTML = '';
+
+        // Adicionar as classes aleatórias ao banner-produtos
+        classesAleatorias.forEach(classe => {
+            const novoLink = document.createElement('a');
+            novoLink.classList.add('categorias');
+            novoLink.href = `products.html?classe=${encodeURIComponent(classe)}`;
+            novoLink.textContent = classe;
+
+            // Criar a imagem baseada no nome da classe
+            const novaImagem = document.createElement('img');
+            novaImagem.src = `../assets/images/${encodeURIComponent(classe)}.webp`;
+            novaImagem.alt = `Imagem da categoria ${classe}`;
+            novoLink.appendChild(novaImagem);
+
+            // Adicionar o evento de clique para abrir o pop-up de filtragem
+            novoLink.addEventListener('click', function(event) {
+                event.preventDefault(); // Prevenir comportamento padrão do link
+                classeEscolhida = classe; // Armazenar a classe escolhida
+                mostrarPopupCategorias(classe, data);
+            });
+
+            bannerProdutosContainer.appendChild(novoLink);
+        });
+
+        // Adicionar o elemento "Todas as categorias" ao final
+        const maisCategoriasDiv = document.createElement('div');
+        maisCategoriasDiv.classList.add('mais-categorias', 'categorias');
+        maisCategoriasDiv.id = 'todasascategorias';
+        maisCategoriasDiv.style.cursor = 'pointer';
+        maisCategoriasDiv.innerHTML = `
+            <div class="categorias-text">
+                <span>Todas as categorias</span>
+            </div>
+            <img src="../assets/images/todasascategorias.webp" alt="Todas as Categorias" id="todasascategorias-img">
+        `;
+        bannerProdutosContainer.appendChild(maisCategoriasDiv);
+
+        // Adicionar evento de clique no elemento "Todas as categorias"
+        maisCategoriasDiv.addEventListener('click', function() {
+            bannerProdutos.style.display = 'none';
+            categoriasContainer.innerHTML = '';
+
+            fetch('https://raw.githubusercontent.com/cimasome/Products/main/Todos%20os%20produtos.json')
+                .then(response => response.json())
+                .then(data => {
+                    const classesCriadas = [];
+                    data.forEach(produto => {
+                        const classe = produto.Classe;
+                        if (!classesCriadas.includes(classe)) {
+                            classesCriadas.push(classe);
+                        }
+                    });
+
+                    classesCriadas.sort((a, b) => a.localeCompare(b));
+
+                    classesCriadas.forEach(classe => {
+                        const novoLink = document.createElement('a');
+                        novoLink.classList.add('categorias');
+                        novoLink.href = '#';
+                        novoLink.textContent = classe;
+
+                        const produto = data.find(produto => produto.Classe === classe);
+                        if (produto && produto.Imagem) {
+                            const novaImagem = document.createElement('img');
+                            novaImagem.src = `../assets/images/${produto.Imagem}.webp`;
+                            novaImagem.alt = `Imagem da categoria ${classe}`;
+                            novoLink.appendChild(novaImagem);
+                        }
+
+                        novoLink.addEventListener('click', function(event) {
+                            event.preventDefault();
+                            classeEscolhida = classe;
+                            mostrarPopupCategorias(classe, data);
+                        });
+
+                        categoriasContainer.appendChild(novoLink);
+
+                        setTimeout(() => {
+                            novoLink.classList.add('exibidas');
+                        }, 10);
+                    });
+
+                    if (categoriasContainer.children.length > 0) {
+                        const mostrarMenosLink = document.createElement('a');
+                        mostrarMenosLink.textContent = 'Mostrar menos';
+                        mostrarMenosLink.href = '#';
+                        mostrarMenosLink.classList.add('categorias', 'mostrar-menos-link');
+                        categoriasContainer.appendChild(mostrarMenosLink);
+
+                        mostrarMenosLink.addEventListener('click', function(event) {
+                            event.preventDefault();
+                            const categorias = categoriasContainer.querySelectorAll('.categorias');
+                            categorias.forEach(categoria => {
+                                categoria.classList.remove('exibidas');
+                            });
+
+                            setTimeout(() => {
+                                categoriasContainer.innerHTML = '';
+                                bannerProdutos.style.display = 'grid';
+                            }, 10);
+                        });
+                    }
+                })
+                .catch(error => {
+                    console.error('Erro ao carregar dados do JSON:', error);
+                });
+        });
+    }
+
+    function mostrarPopupCategorias(classeEscolhida, data) {
+        popupCategoriasContainer.innerHTML = '';
+        verTodosProdutosButton.style.display = 'block';
+
+        const categoriasRelacionadas = data
+            .filter(produto => produto.Classe === classeEscolhida)
+            .map(produto => produto.Categoria);
+
+        const categoriasUnicas = [...new Set(categoriasRelacionadas)];
+
+        categoriasUnicas.forEach(categoria => {
+            const categoriaElemento = document.createElement('a');
+            categoriaElemento.classList.add('popup-category-item');
+            categoriaElemento.href = `products.html?classe=${encodeURIComponent(classeEscolhida)}&categoria=${encodeURIComponent(categoria)}`;
+            categoriaElemento.textContent = categoria;
+            popupCategoriasContainer.appendChild(categoriaElemento);
+        });
+
+        popupCategorias.style.display = 'flex';
+    }
+
+    closePopup.addEventListener('click', function() {
+        popupCategorias.style.display = 'none';
+    });
+
+    verTodosProdutosButton.addEventListener('click', function() {
+        window.location.href = `products.html?classe=${encodeURIComponent(classeEscolhida)}`;
+    });
+
+    popupCategorias.addEventListener('click', function(event) {
+        if (event.target === popupCategorias) {
+            popupCategorias.style.display = 'none';
+        }
+    });
+
+    // Carregar e criar categorias ao iniciar
+    fetch('https://raw.githubusercontent.com/cimasome/Products/main/Todos%20os%20produtos.json')
+        .then(response => response.json())
+        .then(data => {
+            criarCategoriasDinamicamente(data);
+        })
+        .catch(error => {
+            console.error('Erro ao carregar dados do JSON:', error);
+        });
+});
---
+++ Scripts/show_products.js
@@ -0,0 +1,201 @@
+window.onload = function() {
+    loadAndDisplayProducts('https://raw.githubusercontent.com/cimasome/Products/main/Todos%20os%20produtos.json', 'mais-vendidos');
+    loadAndDisplayProducts('https://raw.githubusercontent.com/cimasome/Products/main/infoprodutos.json', 'info-produtos'); // Adicionando o segundo carregamento de produtos
+};
+
+// Variável para controlar a quantidade máxima de itens
+let maxItems = 20;
+
+// Função para carregar dados dos produtos e exibir em um contêiner específico
+function loadAndDisplayProducts(url, containerId) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            // Exibir apenas os itens mais vendidos de cada classe na seção 'mais-vendidos'
+            if (containerId === 'mais-vendidos') {
+                let topProductsByClass = {};
+
+                data.forEach(row => {
+                    const productClass = row['Classe'];
+                    // Garantir que row.Vendas seja tratado como string
+                    const sales = parseInt(row['Vendas'].toString().replace(/\./g, ''));
+
+                    if (!topProductsByClass[productClass] || sales > topProductsByClass[productClass]['Vendas']) {
+                        topProductsByClass[productClass] = { ...row, 'Vendas': sales };
+                    }
+                });
+
+                // Converter o objeto de volta para um array de produtos
+                const topProducts = Object.values(topProductsByClass);
+
+                // Ordenar os produtos em ordem decrescente de vendas
+                topProducts.sort((a, b) => b['Vendas'] - a['Vendas']);
+
+                // Exibir apenas os itens mais vendidos de cada classe
+                displayProducts(topProducts.slice(0, maxItems), containerId);
+            } else {
+                // Exibir todos os produtos na seção 'info-produtos'
+                displayProducts(data, containerId);
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao carregar os produtos:', error);
+            document.getElementById(containerId).textContent = `Erro ao carregar os produtos: ${error.message}`;
+        });
+}
+
+// Função para exibir produtos em um contêiner específico
+function displayProducts(productsData, containerId) {
+    const productsContainer = document.getElementById(containerId);
+    productsContainer.innerHTML = ''; // Limpar conteúdo anterior
+
+    productsData.forEach(row => {
+        const productLink = createProductElement(row);
+        productsContainer.appendChild(productLink);
+    });
+
+    // Adicionar eventos de clique aos novos produtos exibidos
+    addClickEventToProducts();
+}
+
+// Função para criar elemento de produto
+function createProductElement(row) {
+    // Criar o link do produto
+    const productLink = document.createElement('a');
+    productLink.classList.add('produto');
+    productLink.href = row['Link'] ? row['Link'].replace(/\\\//g, '/') : '#'; // Corrigir o formato do link
+    productLink.target = '_blank'; // Abrir em nova guia
+    productLink.style.position = 'relative'; // Adiciona posição relativa para o link
+
+    const nationalityIcon = document.createElement('i');
+    nationalityIcon.classList.add('fa-solid');
+    nationalityIcon.style.position = 'absolute';
+    nationalityIcon.style.top = '15px';
+    nationalityIcon.style.right = '5px';
+    nationalityIcon.style.fontSize = '1.1em';
+    nationalityIcon.style.padding = '2%';
+    nationalityIcon.style.color = '#ea6900'; // Cor do ícone
+    nationalityIcon.style.backgroundColor = '#ffffff'; // Cor de fundo branca
+    nationalityIcon.style.borderBottomLeftRadius = '10px'; // Border radius na borda inferior esquerda
+    nationalityIcon.style.zIndex = '999'; // Adiciona um z-index para garantir que o ícone fique acima de outros elementos
+
+    if (row['Local'] === 'Nacional') {
+        nationalityIcon.classList.add('fa-location-dot'); // ícone para produto nacional
+    } else if (row['Local'] === 'Importado') {
+        nationalityIcon.classList.add('fa-earth-americas'); // ícone para produto importado
+    }
+
+    if (window.matchMedia("(max-width: 428px)").matches) {
+        nationalityIcon.style.left = 'auto'; // Remove o left
+        nationalityIcon.style.top = '5px';
+        nationalityIcon.style.right = '5px'; // Posiciona à direita
+        nationalityIcon.style.margin = '0'; // Remove a margem
+        nationalityIcon.style.padding = '1%'; // Ajusta o padding para uma melhor apresentação em telas menores
+    }
+    // Criar o contêiner principal do produto
+    const productContainer = document.createElement('div');
+    productContainer.classList.add('product-container');
+
+    // Criar a seção para as informações do produto
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+    productInfo.style.textAlign = 'start'; // Alinha todo o conteúdo à esquerda, exceto o título
+
+    // Título do produto
+    const productNameElement = document.createElement('p');
+    productNameElement.classList.add('intra-titulo');
+    const productName = row['Nome'] || 'Nome do Produto';
+    productNameElement.textContent = productName.length > 80 ? productName.slice(0, 80) + '...' : productName;
+
+    // Número de vendidos
+    const productSales = document.createElement('h5');
+    productSales.classList.add('vendidos');
+    // Garantir que row.Vendas seja tratado como string
+    const vendas = parseInt(row['Vendas'].toString().replace(/\./g, ''));
+    const vendasFormatada = vendas.toLocaleString('pt-BR');
+    if (isNaN(vendas) || vendas === 0) {
+        productSales.innerHTML = '<i class="fa-solid fa-dice-d6" style="color: #74C0FC;"></i> 0 Vendidos';
+    } else if (vendas === 1) {
+        productSales.innerHTML = '<i class="fa-solid fa-fire-flame-curved" style="color: #ff0000;"></i> 1 Vendido';
+    } else {
+        productSales.innerHTML = `<i class="fa-solid fa-fire-flame-curved" style="color: #ff0000;"></i> ${vendasFormatada} Vendidos`;
+    }
+
+    // Adicionar os elementos ao DOM
+    productInfo.appendChild(productNameElement);
+    productInfo.appendChild(productSales);
+    
+    // Remover a parte relacionada à criação de preços
+    // priceContainer e seus elementos (originalPrice e productPrice) foram removidos.
+
+    // Imagem do produto
+    const productImage = document.createElement('img');
+    productImage.src = row['URL'] ? decodeURIComponent(row['URL']) : ''; // Decodifica a URL da imagem, se existir
+    productImage.alt = row['Nome'] || 'Produto sem nome';
+    productImage.classList.add('product-image');
+
+    // Adicionar a imagem ao contêiner principal
+    productContainer.appendChild(productImage);
+
+    // Adicionar ícone de nacionalidade ao link do produto
+    productLink.appendChild(nationalityIcon);
+
+    // Adicionar o contêiner do produto às informações do produto
+    productContainer.appendChild(productInfo);
+
+    // Adicionar o contêiner do produto ao link
+    productLink.appendChild(productContainer);
+
+    return productLink;
+}
+
+// Função para adicionar evento de clique aos links de produtos
+function addClickEventToProducts() {
+    const productLinks = document.querySelectorAll('.produto');
+    productLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Evitar que o link abra diretamente
+            const productUrl = this.href; // URL do produto
+            showPopup(productUrl); // Mostrar o pop-up com a URL do produto
+        });
+    });
+}
+
+// Função para exibir o pop-up
+function showPopup(link) {
+    // Verifica se já existe um pop-up ativo
+    if (document.querySelector('.popup')) {
+        return; // Se já existir, não cria um novo
+    }
+
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+
+    const popupContent = document.createElement('div');
+    popupContent.classList.add('popup-content');
+
+    const message = document.createElement('p');
+    message.textContent = 'Você será redirecionado à página do produto. Deseja continuar?';
+
+    const yesButton = document.createElement('button');
+    yesButton.textContent = 'Sim';
+    yesButton.classList.add('yes-button');
+    yesButton.addEventListener('click', function() {
+        window.open(link, '_blank'); // Abre o link em uma nova guia
+        document.body.removeChild(popup); // Remove o pop-up
+    });
+
+    const noButton = document.createElement('button');
+    noButton.textContent = 'Não';
+    noButton.classList.add('no-button');
+    noButton.addEventListener('click', function() {
+        document.body.removeChild(popup); // Remove o pop-up se o usuário clicar em 'Não'
+    });
+
+    popupContent.appendChild(message);
+    popupContent.appendChild(yesButton);
+    popupContent.appendChild(noButton);
+    popup.appendChild(popupContent);
+    document.body.appendChild(popup);
+}
+
